2f62f6c88d17d64211045c968376279bd6d21eed
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-04-04 03:03:31 +0800

    Create tmp.sh

---
+++ game:/StarCraft II/tmp.sh
@@ -0,0 +1,8 @@
+bac(){
+mkdir -p ~/.uni/Alchemy/Singularity/StarCraftII
+btrfs sub snap -r ~/.local/share/Steam ~/.uni/Alchemy/Singularity/StarCraftII/Steam.bac
+}
+
+restore(){
+btrfs sub snap ~/.uni/Alchemy/Singularity/StarCraftII/Steam.bac ~/.local/share/Steam
+}
